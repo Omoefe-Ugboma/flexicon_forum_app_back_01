49a4e7d659f7250ecb8c992c675af747b691b99b
sope-mosuro <sopefrancis@gmail.com>
2023-11-01 23:46:28 +0100

    writing functions for poosting endpoints

---
+++ routes/PostRoutes.js
@@ -0,0 +1,12 @@
+// importing necessary libraries
+const express = require('express')
+const Router = express.Router()
+const {makePosts,getPostById} = require('../controllers/posts')
+
+
+//creating endpoint for posts
+Router.route('/').post(makePosts)
+
+
+// //endpoint for getting all posts
+Router.route('/').get(getPostById)
