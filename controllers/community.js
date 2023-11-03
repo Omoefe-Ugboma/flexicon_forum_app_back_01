71045556ed7ac02eb6766d5910f705529f7dc771
mxsurft <ccunjoku@gmail.com>
2023-11-03 22:46:23 +0100

    created community endpoints2

--- controllers/community.js
+++ controllers/community.js
@@ -1,66 +1,104 @@
 const Comm = require('../models/Community')
-const User = require('../models/User')
+//const User = require('../models/User')
 
-// Define a function to get communities based on a username
+// Define a function to get all communities
 const getComms = async(req, res)=>{
-    const {username} = req.body
-    const user = await User.findOne({username})
-    const {_id:userId} = user
-    const comm = await Comm.find({creator:userId})
+    try{
+        
+        // const {username} = req.body
+        // const user = await User.findOne({username})
+        // const {_id:userId} = user
+        // const comm = await Comm.find({creator:userId})
     
-    //const {name} = req.body
-    //const comm = await Comm.find({name})
-    res.status(201).json({comm})  
+        const comm = await Comm.find()
+        res.status(200).json(comm)
+
+    }catch(error){
+        res.status(500).json({msg: error.message})
+    }   
 
 }
 
 // Define a function to get a community
 const getAComm = async(req, res)=>{
-    // const{user:{userId}, params:{id:commId}} = req
+    try{
+        // const{id:commId} = req.params
+        // const comm = await Comm.findOne({_id: commId})
+        
+        const {name} = req.body
+        if(!name){
+            return res.status(400).json({msg: "Please provide the name of the community"})
+        }
+        const comm = await Comm.findOne({name})
+        if(!comm){
+            return res.status(401).json({msg: `${name} community does not exist`})
+        }
 
-    // const comm = await Comm.findOne({_id: commId,creator:userId})
-    
-    const {name} = req.body
-    const comm = await Comm.findOne({name})
-    if(!comm){
-        return res.status().json('This community does not exist')
-    }
+        res.status(200).json(comm)
 
-    return res.status(200).json({comm})
+    }catch(error){
+        res.status(500).json({msg: error.message})
+    }    
     
 }
 
 // Define a function to create a community
 const createComm = async(req, res)=>{
-    req.body.creator = req.user.userId
-    const comm = await Comm.create(req.body)
-    res.status(201).json({msg:'Community successfully created'})
+    try{
+        const {name} = req.body
+        if(!name){
+            return res.status(400).json({msg: "Please provide a name for the community"})
+        }
+
+        req.body.creator = req.user.userId
+        const comm = await Comm.create(req.body)
+        res.status(201).json(comm)
+
+    }catch(error){
+        res.status(500).json({msg: error.message})
+    }     
     
 }
 
 // Define a function to update a community
 const updateComm = async(req, res)=>{
-    const {body:{name},user:{userId}} = req
-    const comm = await Comm.findOneAndUpdate({name,userId}, {name}, {new:true, runValidators:true})
-    if(!comm){
-        return res.status().json('This community does not exist')
-    }
-    return res.status(201).json({msg:'Update successfully'})
+    try{
 
-    
+        const {body:{name},user:{userId}} = req
+        
+        const comm = await Comm.findOneAndUpdate({name,userId}, {name}, {new:true, runValidators:true})
+        if(!comm){
+            return res.status(401).json({msg: `${name} community does not exist`})
+        }
+        res.status(200).json({msg:'Update successfully'})
+
+    }catch(error){
+        res.status(500).json({msg: error.message})
+    } 
+
 }
 
 // Define a function to delete a community
 const deleteComm = async(req, res)=>{
-    const {body:{name},user:{userId}} = req
-    const comm = await Comm.findOneAndDelete({name,userId})
-    if(!comm){
-        return res.status().json('This community does not exist')
-    }
-    return res.status(201).json({msg:`${name} successfully deleted`})
+    try{
+
+        const {body:{name},user:{userId}} = req
+        
+        if(!name){
+            return res.status(400).json({msg: "Please provide the name of the community"})
+        }
+
+        const comm = await Comm.findOneAndDelete({name,userId})
+        if(!comm){
+            return res.status(401).json({msg: `${name} community does not exist`})
+        }
+        res.status(200).json({msg:`${name} community successfully deleted`})
+
+    }catch(error){
+        res.status(500).json({msg: error.message})
+    }    
     
 }
-
 
 module.exports = {
     getComms,
