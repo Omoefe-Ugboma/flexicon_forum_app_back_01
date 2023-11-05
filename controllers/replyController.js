7e54a83ce2fd1a99f751ac04db5fafa7ec266523
sope-mosuro <sopefrancis@gmail.com>
2023-11-05 20:42:14 +0100

    finishing work on controllers for replies

--- controllers/replyController.js
+++ controllers/replyController.js
@@ -1,103 +1,113 @@
-const Reply = require('../models/Reply')
-const Post = require('../models/Post')
-const User = require('../models/User')
+// Adding necessary imports
+const Reply = require('../models/Reply');
+const Post = require('../models/Post');
+const User = require('../models/User');
+const Thread = require('../models/Thread');
 
-const Thread = require('../models/Thread')
-
+// Function for creating replies
 const createReply = async (req, res) => {
-   const user = req.user.userId;
-   const author = await User.findById({_id:user})
-   const {content,postId,threadId} =req.body
-   const [ post, thread] = await Promise.all([
-    Post.findById({_id:postId}),
-    Thread.findById({_id: threadId})]
-
-   )
+  // Extract the user ID from the request
+  const user = req.user.userId;
   
-   try {
-        const reply = new Reply({
-            author,
-            content,
-            post,
-            thread 
-         })
-          // console.log(reply)
-      
-      const Savedreply = await reply.save();
-       post.replies.push(reply._id)
-       console.log(reply)
-       const savedPost = await post.save()
-       console.log(savedPost)
-        res.json(Savedreply)
-       
+  // Find the author based on the user ID
+  const author = await User.findById({_id: user});
+  
+  // Extract content, postId, and threadId from the request body
+  const { content, postId, threadId } = req.body;
 
-          } catch (error) {
-            res.status(500).json({ message:error.message})
-          }
-        
+  // Find the associated post and thread using their IDs
+  const [ post, thread ] = await Promise.all([
+    Post.findById({_id: postId}),
+    Thread.findById({_id: threadId})
+  ]);
+
+  try {
+    // Create a new reply
+    const reply = new Reply({
+      author,
+      content,
+      post,
+      thread 
+    });
+
+    // Save the reply to the database
+    const savedReply = await reply.save();
+
+    // Update the associated post with the new reply's ID
+    post.replies.push(savedReply._id);
+    const savedPost = await post.save();
+
+    // Send the saved reply as a JSON response
+    res.json(savedReply);
+
+  } catch (error) {
+    // Handle any errors by sending a 500 status code and an error message
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const findReplyById = async (req, res) => {
+  // Extract the reply ID from the request parameters
+  const replyId = req.params.replyId;
+
+  try {
+    // Find the reply by its ID
+    const reply = await Reply.findById(replyId);
+
+    // Check if the reply exists
+    if (reply) {
+      res.json(reply); // Send the reply as a JSON response
+    } else {
+      res.status(404).json({ message: 'Reply not found' }); // Send a 404 status if reply is not found
     }
 
-    const findReplyById = async (req, res) => {
-      const replyId =req.params.replyId
-      console.log(replyId)
-      try {
-     const reply = await Reply.findById(replyId)
-        if (reply) {
-          res.json(reply)
-        } else {
-          res.status(404).json({ message: 'Reply not found' })
-        }
-      } catch (error) {
-        res.status(500).json({ message: 'Error fetching reply' })
-      }
-    };
-    const updateReply = async (req, res) => {
-      const replyId = req.params.replyId
-      const {content,threadId,postId}=req.body
-      try {
-        const reply = await Reply.findById(replyId)
-        if (!reply) {
-          return res.status(404).json({ message: 'Reply not found' })
-        }
-        reply.content = content ? content: reply.content
-        reply.thread = threadId ? threadId: reply.thread
-        reply.post = postId? postId: reply.post
-        
-        
-        const updateReply = await Reply.save()
-        res.status(202).json(updateReply)
-      } catch (error) {
-        res.status(500).json({ message: 'Error updating reply' })
-      }
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching reply' }); // Handle any errors
+  }
+};
+
+const updateReplyById = async (req, res) => {
+  // Extract the reply ID and content from the request
+  const replyId = req.params.replyId;
+  const { content } = req.body;
+
+  try {
+    // Find the reply by its ID
+    const reply = await Reply.findById(replyId);
+
+    if (!reply) {
+      return res.status(404).json({ message: 'Reply not found' }); // Send a 404 status if reply is not found
     }
 
-     const deleteReplyById = async (req, res) => {
-      const replyId = req.params.replyId
+    // Update the content if provided in the request body
+    reply.content = content ? content : reply.content;
 
-      try {
-        // Find and delete the reply by its ID
-        const reply = await Reply.findByIdAndDelete(replyId)
-    
-        if (!reply) {
-          // If the reply does not exist, send a 404 status code and a message
-          return res.status(404).json({ message: 'Reply not found' })
-        }
-    
-        // Send a success message as a JSON response
-        res.json({ message: 'Reply deleted successfully' })
-      } catch (error) {
-        // Handle any errors by sending a 500 status code and an error message
-        res.status(500).json({ message: 'Error deleting Reply' })
-      }
-    };
-  module.exports = {createReply,findReplyById,deleteReplyById}
+    // Save the updated reply
+    const updatedReply = await reply.save();
 
+    res.status(202).json(updatedReply); // Send the updated reply as a JSON response
 
+  } catch (error) {
+    res.status(500).json({ message: 'Error updating reply' }); // Handle any errors
+  }
+};
 
+const deleteReplyById = async (req, res) => {
+  const replyId = req.params.replyId;
 
-  //    const savedReply=await reply.save()
-        //    res.json(savedReply)
-        //   post.replies.push(savedReply)
-        //   console.log(savedReply)
-        //   const savedPost = await post.save()
-        //    res.json(reply)
+  try {
+    // Find and delete the reply by its ID
+    const reply = await Reply.findByIdAndDelete(replyId);
+
+    if (!reply) {
+      return res.status(404).json({ message: 'Reply not found' }); // Send a 404 status if reply is not found
+    }
+
+    res.json({ message: 'Reply deleted successfully' }); // Send a success message as a JSON response
+
+  } catch (error) {
+    res.status(500).json({ message: 'Error deleting reply' }); // Handle any errors
+  }
+};
+
+module.exports = { createReply, findReplyById, deleteReplyById, updateReplyById };
--- routes/replyRoutes.js
+++ routes/replyRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const {createReply,findReplyById,deleteReplyById} =require('../controllers/replyController');
+const {createReply,findReplyById,deleteReplyById,updateReplyById} =require('../controllers/replyController');
 const Router = express.Router();
 
 
 Router.route('/').post(createReply);
 Router.route('/:replyId').get(findReplyById);
 Router.route('/:replyId').delete(deleteReplyById);
+Router.route('/:replyId').put(updateReplyById);
 module.exports = Router
