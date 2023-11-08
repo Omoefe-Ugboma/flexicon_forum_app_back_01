578a78308e3bec0be948182abd263740aa234660
adelaykay <adelekeolasope@gmail.com>
2023-11-08 08:47:22 +0100

    Minor changes to fix conflict

---
+++ middleware/jwt.js
@@ -0,0 +1,27 @@
+const Jwt = require('jsonwebtoken')
+
+// Create user's access token
+const createToken = async userId => {
+  return Jwt.sign({ userId, name: this.username }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_LIFETIME,
+  })
+}
+
+// Verify user's access token
+const verifyUser = async (req, res, next) => {
+  const token = req.header('Authorization').substring(7)
+  if (!token) {
+    return res.status(401).json({ message: 'Access denied. Token missing.' })
+  }
+
+  // Token verification
+  try {
+    const decodedToken = Jwt.verify(token, process.env.JWT_SECRET)
+    req.user = decodedToken // Store the decoded token in the request for further use
+    next() // Token is valid, proceed to the next middleware or route handler
+  } catch (err) {
+    return res.status(403).json({ message: 'Access denied. Invalid token.' })
+  }
+}
+
+module.exports = { createToken, verifyUser }
