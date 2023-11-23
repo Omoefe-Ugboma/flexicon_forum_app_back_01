501888699c1a5e18ec547ce4fb717e194316b9a7
sope-mosuro <sopefrancis@gmail.com>
2023-11-23 16:32:23 +0100

    making changes requested by Mr Adeleke

--- controllers/searchControllers.js
+++ controllers/searchControllers.js
@@ -6,23 +6,25 @@ const Community = require('../models/Community');
 // Define the Search function
 const Search = async (req, res) => {
   // Extract the 'query' parameter from the request
-  const { query } = req.query;
-
-  // Log the query to the console for debugging purposes
-  console.log(query);
-
-  // Create a case-insensitive regex for the query
-  const regexQuery = new RegExp(query, 'i');
+  const{ query,limit} = req.query;
+  const trimquery = query.replace(/\s/g, '');
+  console.log(trimquery);
+    // Use a default limit if not provided
+    const resultLimit = limit ? parseInt(limit) : 10;
+    //handle empty requests
+  if (!trimquery) {
+    return res.status(400).json({message: 'A search term is required'})
+  }
 
   try {
     // Search for posts with the case-insensitive regex
-    const postResults = await Post.find({ $text: { $search: query, $caseSensitive: false } });
+    const postResults = await Post.find({ $text: { $search: trimquery, $caseSensitive: false } }).limit(resultLimit)
 
     // Search for communities with the case-insensitive regex
-    const communitiesResults = await Community.find({ $text: { $search: query, $caseSensitive: false } });
+    const communitiesResults = await Community.find({ $text: { $search: trimquery, $caseSensitive: false } }).limit(resultLimit)
 
     // Search for users with the case-insensitive regex
-    const userResults = await User.find({ $text: { $search: query, $caseSensitive: false } });
+    const userResults = await User.find({ $text: { $search: trimquery, $caseSensitive: false } }).limit(resultLimit)
 
     // Combine the results from different models into an object
     const allResults = {
@@ -36,7 +38,7 @@ const Search = async (req, res) => {
 
     if (noMatches) {
       // Return a response if no matches are found
-      return res.status(200).json({ message: 'No matches found for the given query' });
+      return res.status(404).json({ message: 'No matches found for the given query' });
     }
 
     // Return the results if there are matches
